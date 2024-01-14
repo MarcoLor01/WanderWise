4ba37f5c260cb453819ccba8d06acceef0025de9
MarcoLor01 <marcolorenzini23@gmail.com>
2024-01-14 21:08:19 +0100

    Day commit

--- src/main/java/com/example/wanderwisep/dao/TicketDAOCSV.java
+++ src/main/java/com/example/wanderwisep/dao/TicketDAOCSV.java
@@ -33,11 +33,16 @@ public class TicketDAOCSV implements TicketDAO {
 
     public TicketDAOCSV() throws IOException {
         this.fd = new File(CSV_FILE_NAME);
-
         if (!fd.exists()) {
-            fd.createNewFile();
+            boolean fileCreated = fd.createNewFile();
+            if (fileCreated) {
+                System.out.println("File created: " + CSV_FILE_NAME);
+            } else {
+                System.out.println("File already exists: " + CSV_FILE_NAME);
+            }
         }
     }
+
 
     @Override
     public void createTicket(Ticket ticket) throws DAOException, SQLException, IOException, DuplicateTourException, CsvValidationException {
@@ -55,7 +60,7 @@ public class TicketDAOCSV implements TicketDAO {
 
     private static synchronized void insertTicket(File fd, Ticket ticket) throws IOException {
 
-        try (CSVWriter csvWriter = new CSVWriter(new BufferedWriter(new FileWriter(fd, true)))) {
+        try (CSVWriter csvWriter = new CSVWriter(new BufferedWriter(new FileWriter(fd, true)))) { //Security Hotspot
             String[] ticketRecord = new String[9];
             ticketRecord[INDEX_ID_TICKET] = ticket.getIdTicket();
             ticketRecord[INDEX_STATE] = ticket.getState().getStateName();
@@ -73,121 +78,127 @@ public class TicketDAOCSV implements TicketDAO {
     }
 
     private static synchronized List<Ticket> retrieveByIdTicket(File fd, String idTicket) throws IOException, CsvValidationException, TicketNotFoundException {
-        CSVReader csvReader = new CSVReader(new BufferedReader(new FileReader(fd)));
-        String[] record;
-        List<Ticket> ticketList = new ArrayList<>();
+        try (CSVReader csvReader = new CSVReader(new BufferedReader(new FileReader(fd)))) {
+            String[] record;
+            List<Ticket> ticketList = new ArrayList<>();
 
-        while ((record = csvReader.readNext()) != null) {
-            boolean recordFound = record[INDEX_ID_TICKET].equals(String.valueOf(idTicket));
-            if (recordFound) {
-                String id = String.valueOf(record[INDEX_ID_TICKET]);
-                stateEnum state = stateEnum.fromString(record[INDEX_STATE]);
-                LocalDate prenotationDate = LocalDate.parse(String.valueOf(record[INDEX_PRENOTATION_DATE]));
-                String user = String.valueOf(record[INDEX_USER]);
-                String touristGuide = String.valueOf(record[INDEX_TOURIST_GUIDE]);
-                String guidedTour = String.valueOf(record[INDEX_MY_GUIDED_TOUR_ID]);
-                LocalDate departureDate = LocalDate.parse(String.valueOf(record[INDEX_DEPARTURE_DATE]));
-                LocalDate returnDate = LocalDate.parse(String.valueOf(record[INDEX_RETURN_DATE]));
-                String nameTour = String.valueOf(record[INDEX_NAME_TOUR]);
-                Ticket ticket = new Ticket(id, state, prenotationDate, user, touristGuide, guidedTour, departureDate, returnDate, nameTour);
-                ticketList.add(ticket);
+            while ((record = csvReader.readNext()) != null) {
+                boolean recordFound = record[INDEX_ID_TICKET].equals(String.valueOf(idTicket));
+                if (recordFound) {
+                    String id = String.valueOf(record[INDEX_ID_TICKET]);
+                    stateEnum state = stateEnum.fromString(record[INDEX_STATE]);
+                    LocalDate prenotationDate = LocalDate.parse(String.valueOf(record[INDEX_PRENOTATION_DATE]));
+                    String user = String.valueOf(record[INDEX_USER]);
+                    String touristGuide = String.valueOf(record[INDEX_TOURIST_GUIDE]);
+                    String guidedTour = String.valueOf(record[INDEX_MY_GUIDED_TOUR_ID]);
+                    LocalDate departureDate = LocalDate.parse(String.valueOf(record[INDEX_DEPARTURE_DATE]));
+                    LocalDate returnDate = LocalDate.parse(String.valueOf(record[INDEX_RETURN_DATE]));
+                    String nameTour = String.valueOf(record[INDEX_NAME_TOUR]);
+                    Ticket ticket = new Ticket(id, state, prenotationDate, user, touristGuide, guidedTour, departureDate, returnDate, nameTour);
+                    ticketList.add(ticket);
+                }
             }
-        }
-        csvReader.close();
 
-        if (ticketList.isEmpty()) {
-            throw new TicketNotFoundException("No Ticket founded with ticket id: " + idTicket);
+            if (ticketList.isEmpty()) {
+                throw new TicketNotFoundException("No Ticket founded with ticket id: " + idTicket);
+            }
+
+            return ticketList;
         }
-        return ticketList;
     }
+
 
     @Override
     public List<Ticket> retrieveTicket(String userEmail) throws SQLException, TicketNotFoundException, CsvValidationException, IOException {
         return retrieveByUserName(this.fd, userEmail);
     }
 
-    public List<Ticket> retrieveByUserName(File fd, String userName) throws IOException, CsvValidationException, TicketNotFoundException {
-        CSVReader csvReader = new CSVReader(new BufferedReader(new FileReader(fd)));
-        String[] record;
+    private static synchronized List<Ticket> retrieveByUserName(File fd, String userName) throws IOException, CsvValidationException, TicketNotFoundException {
+        try (CSVReader csvReader = new CSVReader(new BufferedReader(new FileReader(fd)))) {
+            String[] record;
 
-        List<Ticket> ticketList = new ArrayList<>();
+            List<Ticket> ticketList = new ArrayList<>();
 
-        while ((record = csvReader.readNext()) != null) {
-            boolean recordFound = record[INDEX_USER].equals(userName);
-            if (recordFound) {
-                String id = String.valueOf(record[INDEX_ID_TICKET]);
-                stateEnum state = stateEnum.fromString(record[INDEX_STATE]);
-                LocalDate prenotationDate = LocalDate.parse(String.valueOf(record[INDEX_PRENOTATION_DATE]));
-                String user = String.valueOf(record[INDEX_USER]);
-                String touristGuide = String.valueOf(record[INDEX_TOURIST_GUIDE]);
-                String guidedTour = String.valueOf(record[INDEX_MY_GUIDED_TOUR_ID]);
-                LocalDate departureDate = LocalDate.parse(String.valueOf(record[INDEX_DEPARTURE_DATE]));
-                LocalDate returnDate = LocalDate.parse(String.valueOf(record[INDEX_RETURN_DATE]));
-                String nameTour = String.valueOf(record[INDEX_NAME_TOUR]);
-                Ticket ticket = new Ticket(id, state, prenotationDate, user, touristGuide, guidedTour, departureDate, returnDate, nameTour);
-                ticketList.add(ticket);
+            while ((record = csvReader.readNext()) != null) {
+                boolean recordFound = record[INDEX_USER].equals(userName);
+                if (recordFound) {
+                    String id = String.valueOf(record[INDEX_ID_TICKET]);
+                    stateEnum state = stateEnum.fromString(record[INDEX_STATE]);
+                    LocalDate prenotationDate = LocalDate.parse(String.valueOf(record[INDEX_PRENOTATION_DATE]));
+                    String user = String.valueOf(record[INDEX_USER]);
+                    String touristGuide = String.valueOf(record[INDEX_TOURIST_GUIDE]);
+                    String guidedTour = String.valueOf(record[INDEX_MY_GUIDED_TOUR_ID]);
+                    LocalDate departureDate = LocalDate.parse(String.valueOf(record[INDEX_DEPARTURE_DATE]));
+                    LocalDate returnDate = LocalDate.parse(String.valueOf(record[INDEX_RETURN_DATE]));
+                    String nameTour = String.valueOf(record[INDEX_NAME_TOUR]);
+                    Ticket ticket = new Ticket(id, state, prenotationDate, user, touristGuide, guidedTour, departureDate, returnDate, nameTour);
+                    ticketList.add(ticket);
+                }
             }
+
+            if (ticketList.isEmpty()) {
+                throw new TicketNotFoundException("No Ticket Found matching with name: " + userName);
+            }
+
+            return ticketList;
         }
-
-        csvReader.close();
-
-        if (ticketList.isEmpty()) {
-            throw new TicketNotFoundException("No Ticket Found matching with name: " + userName);
-        }
-
-        return ticketList;
     }
+
 
     @Override
     public List<Ticket> retrieveTicketForGuide(String touristGuide) throws SQLException, TicketNotFoundException, CsvValidationException, IOException {
         return retrieveTicketFromTourGuideName(this.fd, touristGuide);
     }
 
-    public List<Ticket> retrieveTicketFromTourGuideName(File fd, String touristGuideName) throws IOException, CsvValidationException, TicketNotFoundException {
-        CSVReader csvReader = new CSVReader(new BufferedReader(new FileReader(fd)));
-        String[] record;
+    private static synchronized List<Ticket> retrieveTicketFromTourGuideName(File fd, String touristGuideName) throws IOException, CsvValidationException, TicketNotFoundException {
+        try (CSVReader csvReader = new CSVReader(new BufferedReader(new FileReader(fd)))) {
+            String[] record;
 
-        List<Ticket> ticketList = new ArrayList<>();
+            List<Ticket> ticketList = new ArrayList<>();
 
-        while ((record = csvReader.readNext()) != null) {
-            boolean recordFound = record[INDEX_TOURIST_GUIDE].equals(touristGuideName);
-            if (recordFound) {
-                String id = String.valueOf(record[INDEX_ID_TICKET]);
-                stateEnum state = stateEnum.fromString(record[INDEX_STATE]);
-                LocalDate prenotationDate = LocalDate.parse(String.valueOf(record[INDEX_PRENOTATION_DATE]));
-                String user = String.valueOf(record[INDEX_USER]);
-                String touristGuide = String.valueOf(record[INDEX_TOURIST_GUIDE]);
-                String guidedTour = String.valueOf(record[INDEX_MY_GUIDED_TOUR_ID]);
-                LocalDate departureDate = LocalDate.parse(String.valueOf(record[INDEX_DEPARTURE_DATE]));
-                LocalDate returnDate = LocalDate.parse(String.valueOf(record[INDEX_RETURN_DATE]));
-                String nameTour = String.valueOf(record[INDEX_NAME_TOUR]);
-                Ticket ticket = new Ticket(id, state, prenotationDate, user, touristGuide, guidedTour, departureDate, returnDate, nameTour);
-                ticketList.add(ticket);
+            while ((record = csvReader.readNext()) != null) {
+                boolean recordFound = record[INDEX_TOURIST_GUIDE].equals(touristGuideName);
+                if (recordFound) {
+                    String id = String.valueOf(record[INDEX_ID_TICKET]);
+                    stateEnum state = stateEnum.fromString(record[INDEX_STATE]);
+                    LocalDate prenotationDate = LocalDate.parse(String.valueOf(record[INDEX_PRENOTATION_DATE]));
+                    String user = String.valueOf(record[INDEX_USER]);
+                    String touristGuide = String.valueOf(record[INDEX_TOURIST_GUIDE]);
+                    String guidedTour = String.valueOf(record[INDEX_MY_GUIDED_TOUR_ID]);
+                    LocalDate departureDate = LocalDate.parse(String.valueOf(record[INDEX_DEPARTURE_DATE]));
+                    LocalDate returnDate = LocalDate.parse(String.valueOf(record[INDEX_RETURN_DATE]));
+                    String nameTour = String.valueOf(record[INDEX_NAME_TOUR]);
+                    Ticket ticket = new Ticket(id, state, prenotationDate, user, touristGuide, guidedTour, departureDate, returnDate, nameTour);
+                    ticketList.add(ticket);
+                }
             }
+
+            if (ticketList.isEmpty()) {
+                throw new TicketNotFoundException("No Ticket Found matching with name: " + touristGuideName);
+            }
+
+            return ticketList;
         }
-
-        csvReader.close();
-
-        if (ticketList.isEmpty()) {
-            throw new TicketNotFoundException("No Ticket Found matching with name: " + touristGuideName);
-        }
-
-        return ticketList;
     }
+
 
     public void retrieveTicketFromTourGuide(String touristGuide, String userEmail, String idTour, String decision) throws CsvValidationException, IOException {
         retrieveFromTourGuide(this.fd, touristGuide, userEmail, idTour, decision);
     }
 
     public void retrieveFromTourGuide(File fd, String touristGuide, String userEmail, String idTour, String decision) throws CsvValidationException, IOException {
-        CSVReader csvReader = new CSVReader(new BufferedReader(new FileReader(fd)));
-        String[] record;
-
-        while ((record = csvReader.readNext()) != null) {
-            boolean recordFound = record[INDEX_USER].equals(userEmail) & record[INDEX_TOURIST_GUIDE].equals(touristGuide) & record[INDEX_ID_TICKET].equals(idTour) & record[INDEX_STATE].equals("WAITING");
-            if (recordFound) {
-                record[INDEX_STATE] = decision;
+        try (CSVReader csvReader = new CSVReader(new BufferedReader(new FileReader(fd)))) {
+            String[] record;
+            while ((record = csvReader.readNext()) != null) {
+                boolean recordFound = record[INDEX_USER].equals(userEmail) &&
+                        record[INDEX_TOURIST_GUIDE].equals(touristGuide) &&
+                        record[INDEX_ID_TICKET].equals(idTour) &&
+                        record[INDEX_STATE].equals("WAITING");
+                if (recordFound) {
+                    record[INDEX_STATE] = decision;
+                }
             }
         }
     }
+
 }
